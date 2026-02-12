6b02b95e4fb33e7f417412fc8d88f006b1389d3a
Peter David <davidpeterjan@gmail.com>
2026-02-12 17:09:20 +0800

    style: log in done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next'
 import { LoginForm } from '@/components/auth/login-form'
 
 export const metadata: Metadata = {
-  title: 'Login | Participay',
-  description: 'Sign in to your Participay account',
+  title: 'Login | ParticiPay',
+  description: 'Sign in to your ParticiPay account',
 }
 
 export default function LoginPage() {
